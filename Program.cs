068d96e05e8ae2c42de00ba393b71eca73000a7a
pepolx <patryk.prange@gmail.com>
2023-11-17 02:23:57 +0100

    Test

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ class Program
         {
             Console.WriteLine(card.Suit + "   " + card.Face + "    " + card.Symbol +
                               "    " + card.Value);
+            
         }        
 
     }
